fdd337728dd9d7d7fdb400c034b12bd76173047a
Tina Reymann <info@raspberryblack.com>
2012-06-24 15:55:50 +0930

    Tweaked animation to look better

--- squeezebar.js
+++ squeezebar.js
@@ -1,31 +1,28 @@
 (function ($) {
 	
-	console.log(Drupal.settings.squeezebar.key);
+	var delay = Drupal.settings.squeezebar.delay;
 
 	var stub_showing = false;
 	
 	function squeezebar_show() { 
 	    if(stub_showing) {
 	     	$('.squeezebar-stub').slideUp('fast', function() {
-				$('.squeezebar').show(/* 'bounce', { times:3, distance:15 },*/ 100); 
-				$('body').animate({ "marginTop": "2.4em" }, 100);
+				$('.squeezebar').slideDown('fast'); 
+				$('body').animate({ "marginTop": "2.4em" }, 'fast');
 			}); 
 	    }
 	    else {
-	      	$('.squeezebar').show(/* 'bounce', { times:3, distance:15 },*/ 100); 
-	      	$('body').animate({"marginTop": "2.4em"}, 100);
+	      	$('.squeezebar').slideDown('fast'); 
+	      	$('body').animate({"marginTop": "2.4em"}, 'fast');
 	    }
 	}
 	
-	function squeezebar_hide() { 
+	function squeezebar_hide() {
+		$('body').animate({"marginTop": "0px"}, 'fast'); 
 	    $('.squeezebar').slideUp('fast', function() {
-	    	$('.squeezebar-stub').show(/* 'bounce', { times:3, distance:15 }, */ 100);  
+	    	$('.squeezebar-stub').slideDown('fast'); 
 	    	stub_showing = true;
 	    }); 
-	
-	    if( $(window).width() > 1024 ) {
-	      	$('body').animate({"marginTop": "0px"}, 100); // if width greater than 1024 pull up the body
-	    }
 	}
 	
 	$().ready(function() {
@@ -34,7 +31,7 @@
 	
 	    window.setTimeout(function() {
 	    	squeezebar_show();
-	    }, 1000);
+	    }, delay);
 	});
 
 	
